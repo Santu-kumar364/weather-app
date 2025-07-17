bdfc3b8a5d23d9800d8047c21f5e142bc40b42ab
Santu-kumar364 <santukumar7619@gmail.com>
2025-07-17 21:15:28 +0530

    add files

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,57 +1,57 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './Weather.css';
-import search_icon from '../assets/searchw.png';
-import clear_icon from '../assets/weather.png';
-import humidity_icon from '../assets/humidity.png';
-import wind_icon from '../assets/storm.png';
-import rain_showers from '../assets/img5.png';
-import thunderstorms from '../assets/img1.avif';
-import light_rain from '../assets/img4.png';
-import partly_cloudly from '../assets/img2.webp';
-import mist_icon from '../assets/mist.webp';
-import cloudy_icon from '../assets/cloudy.webp';
- 
- 
+import React, { useEffect, useState, useRef } from "react";
+import "./Weather.css";
+import search_icon from "../assets/searchw.png";
+import clear_icon from "../assets/weather.png";
+import humidity_icon from "../assets/humidity.png";
+import wind_icon from "../assets/storm.png";
+import rain_showers from "../assets/img5.png";
+import thunderstorms from "../assets/img1.avif";
+import light_rain from "../assets/img4.png";
+import partly_cloudly from "../assets/img2.webp";
+import mist_icon from "../assets/mist.webp";
+import cloudy_icon from "../assets/cloudy.webp";
+
 const Weather = () => {
   const inputRef = useRef();
-  const [weatherData, setWeatherData] = useState(null);  
+  const [weatherData, setWeatherData] = useState(null);
 
-  const allIcons = { 
-    "1000": clear_icon,  
-    "1003": partly_cloudly,
-    "1006": cloudy_icon, 
-    "1009": partly_cloudly, 
-    "1030": mist_icon, 
-    "1063": rain_showers,
-    "1183": light_rain,
-    "1240": light_rain,  
-    "1276": thunderstorms
+  const allIcons = {
+    1000: clear_icon,
+    1003: partly_cloudly,
+    1006: cloudy_icon,
+    1009: partly_cloudly,
+    1030: mist_icon,
+    1063: rain_showers,
+    1183: light_rain,
+    1240: light_rain,
+    1276: thunderstorms,
   };
 
   const search = async (city) => {
-
-    if(city === "") {
+    if (city === "") {
       alert("Enter City Name");
       return;
     }
 
     try {
-      const url = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${city}`;
+      const url = `http://api.weatherapi.com/v1/current.json?key=${
+        import.meta.env.VITE_API_KEY
+      }&q=${city}`;
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);    // it will display data into console 
-      if(!response.ok) {
+      console.log(data); // it will display data into console
+      if (!response.ok) {
         alert("city not found");
         return;
       }
       const iconCode = data.current.condition.code.toString();
-      const icon = allIcons[iconCode] || clear_icon; // given icons from my side 
+      const icon = allIcons[iconCode] || clear_icon; // given icons from my side
 
       setWeatherData({
         humidity: data.current.humidity,
         windSpeed: data.current.wind_kph,
         temperature: Math.floor(data.current.temp_c),
-        location: `${data.location.name}, ${data.location.country}`,  // want country name along with city
+        location: `${data.location.name}, ${data.location.country}`, // want country name along with city
         icon: icon,
       });
     } catch (error) {
@@ -64,18 +64,36 @@ const Weather = () => {
   }, []);
 
   return (
-    <div className='weather'>
+    <div className="weather">
       <div className="search-bar">
-        <input ref={inputRef} type="text" placeholder='Search' />
-        <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)} />
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") search(inputRef.current.value);
+          }}
+        />
+        <img
+          src={search_icon}
+          alt=""
+          onClick={() => search(inputRef.current.value)}
+        />
       </div>
 
       {/* using Ternary operator  */}
       {weatherData ? (
         <>
-          <img src={weatherData.icon} alt="Weather Icon" className='weather-icon' />
-          <p className='temperature'>{weatherData.temperature}{"\u00B0"}c</p>
-          <p className='location'>{weatherData.location}</p>
+          <img
+            src={weatherData.icon}
+            alt="Weather Icon"
+            className="weather-icon"
+          />
+          <p className="temperature">
+            {weatherData.temperature}
+            {"\u00B0"}c
+          </p>
+          <p className="location">{weatherData.location}</p>
 
           <div className="weather-data">
             <div className="col">
